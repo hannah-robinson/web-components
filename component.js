81059c1a74d6d451dbe43077533509aea2e1bbfe
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-09-04 15:41:23 +1200

    Parse an HTML string

--- component.js
+++ component.js
@@ -8,13 +8,20 @@ class WebComponent extends HTMLElement {
     // <div>
     //   <span>This is a web component</span>
     // </div>`
-    // Creating elements and addiing to DOM
-    const div = document.createElement('div')
-    const span = document.createElement('span')
-    span.innerHTML = 'This is a web component'
-    div.appendChild(span)
-    this.appendChild(div)
+
+    // Creating elements and adding to DOM
+    // const div = document.createElement('div')
+    // const span = document.createElement('span')
+    // span.innerHTML = 'This is a web component'
+    // div.appendChild(span)
+    // this.appendChild(div)
     // "this" is the web component itself
+
+    // Parse an HTML string
+    const html = `<div><span>This is a web component</span></div>`
+    const innerHTML = new DOMParser().parseFromString(html, 'text/html').body
+      .innerHTML
+    this.innerHTML = innerHTML
   }
 }
 
