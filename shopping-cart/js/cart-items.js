4a3e7fb3cd011b91532ba48fd94199960b6a750d
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-01-06 16:51:48 +1300

    Add custom event for typed in quantity

--- shopping-cart/js/cart-items.js
+++ shopping-cart/js/cart-items.js
@@ -7,7 +7,7 @@ class CartItems extends HTMLElement {
     window.cart.items[index].qty = value
     this.render()
   }
-  //  We want to trigger a change event on the quantity. We are using a custom event to pass through the line of the item we want to change and its new quantity to a new method and then that method is going to updatet cart.items. We could have put it inside the QuantityInput component, but it makes more sense to the instructor to put it in cart-items because it is literally changing cart.items, the object that this component relates to.
+  //  We want to trigger a change event on the quantity. We are using a custom event to pass through the line of the item we want to change and its new quantity to a new method and then that method is going to update cart.items. We could have put it inside the QuantityInput component, but it makes more sense to the instructor to put it in cart-items because it is literally changing cart.items, the object that this component relates to. We aer uisng a cuustsom event because there are two ways to change the quantity value (plus/minus buttons and typing in a new number)
 
   render() {
     this.innerHTML = '' // To reset old info when cart is rerendered.
@@ -18,7 +18,8 @@ class CartItems extends HTMLElement {
       const line_price = item.price * item.qty
       // We use the += below to append a table row
       // Input set type to "number" so that nothing else can be input and min to 0 so that we don't get negative numbers
-      table.innerHTML += `
+      if (item.qty != 0) {
+        table.innerHTML += `
         <tr>
           <td>${item.name}</td>
           <td style="text-align:center;">
@@ -37,6 +38,7 @@ class CartItems extends HTMLElement {
           <td style="text-align:right;">${line_price}</td>
         </tr>
       `
+      }
     })
     table_container.appendChild(table)
     this.appendChild(table_container) // 'this' is the web component
@@ -49,15 +51,17 @@ class CartItems extends HTMLElement {
     )
     // in a real store that has prices with decimals, you'd need parseFloat.
 
-    footer.innerHTML = `
-    <table class="cart-footer">
-      <tr>
-        <td>Total:</td>
-        <td style="text-align: right;">$${cart_total}</td>
-      </tr>
-    </table>
-    `
-    this.appendChild(footer)
+    if (cart_total != 0) {
+      footer.innerHTML = `
+      <table class="cart-footer">
+        <tr>
+          <td>Total:</td>
+          <td style="text-align: right;">$${cart_total}</td>
+        </tr>
+      </table>
+      `
+      this.appendChild(footer)
+    }
   }
   // The render method is outside the constructor function because we don't want it to automatically run when the component is rendered. We want it to render and rerender when we specifically ask it to.
 }
--- shopping-cart/js/qty-input.js
+++ shopping-cart/js/qty-input.js
@@ -2,20 +2,29 @@ class QuantityInput extends HTMLElement {
   constructor() {
     super()
     this.cartItems = document.querySelector('cart-items')
+    this.changeEvent = new Event('change', { bubbles: true })
+    // bubbling registers a change event on the parent web component as well
 
     this.input = this.querySelector('input')
     this.querySelectorAll('button').forEach((button) => {
       button.addEventListener('click', this.onButtonClick.bind(this))
     })
     // We bind 'this' so that 'this' represents the web component and not the event
+
+    this.addEventListener('change', (event) => {
+      const input = event.currentTarget.querySelector('input')
+      this.cartItems.updateQty(input.dataset.index, input.value)
+      // allows us to access data-index="${index}" which cart-items.js inserts into the HTML and is available on the dataset array.
+    })
   }
 
   onButtonClick(event) {
     event.preventDefault()
     const previousValue = this.input.value
     event.target.name == 'plus' ? this.input.stepUp() : this.input.stepDown()
-    this.cartItems.updateQty(this.input.dataset.index, this.input.value)
-    // allows us to access data-index="${index}" which cart-items.js inserts into the HTML and is available on the dataset array.
+    if (previousValue != this.input.value)
+      this.input.dispatchEvent(this.changeEvent)
+    // if statement is just an extra check
   }
 }
 
